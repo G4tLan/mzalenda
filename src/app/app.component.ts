e258d83af55e10f609d9f21d55688b668b308d2c
G4tLan <langagift01@gmail.com>
2024-08-22 12:00:37 +0200

    dialog init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -30,8 +30,13 @@ export class AppComponent {
     2024,
     Array.from({length: Math.floor(Math.random() * 17)}, (s,gui) => new Story(`m-${i}-${gui}`,
       `https://picsum.photos/seed/random=${i+gui+1}/300/300`,
-      "story",
-      ["https://www.sample.com/?crown=building", "http://www.pan.sample.edu/shake/tongue?power=exchange#jeans"]
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      (() => {
+        const a = Math.floor(Math.random() * 4);
+        const b = Math.floor(Math.random() * 5) ?? 1;
+
+        return ["https://www.sample.com/?crown=building", "http://www.pan.sample.edu/shake/tongue?power=exchange#jeans", "#MZalenda", "Musi Khawume", "#Hulio Ima"].slice(a,b)
+      })()
     ))
   ));
 
--- src/app/components/month/month.component.ts
+++ src/app/components/month/month.component.ts
@@ -1,14 +1,35 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { MonthData } from '../../structures/models';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  Input,
+  OnInit,
+} from '@angular/core';
+import { MonthData, Story } from '../../structures/models';
+import {
+  MAT_DIALOG_DATA,
+  MatDialog,
+  MatDialogModule,
+} from '@angular/material/dialog';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { Overlay } from '@angular/cdk/overlay';
+
+export interface DialogData {
+  stories: Array<Story>;
+  name: string,
+  id: string,
+  year: string
+}
 
 @Component({
   selector: 'app-month',
   standalone: true,
-  imports: [],
+  imports: [MatDialogModule],
   templateUrl: './month.component.html',
   styleUrl: './month.component.scss',
   host: {
-    "style": "display: block; width: 100%; height: 100%; max-width: 300px",
+    "month": "main"
   }
 })
 export class MonthComponent implements OnInit {
@@ -18,11 +39,12 @@ export class MonthComponent implements OnInit {
 
   _imagePosMap: { [index: number]: string } = {};
 
-  constructor() {
-  }
+  readonly dialog = inject(MatDialog);
+  readonly scroll = inject(Overlay);
+
+  constructor() {}
   ngOnInit(): void {
-    
-    this.imagPos()
+    this.imagPos();
   }
 
   imagPos() {
@@ -35,27 +57,61 @@ export class MonthComponent implements OnInit {
       let rowSpan, colSpan;
       rowSpan = rowEnd + 1 - rowStart;
       colSpan = colEnd + 1 - colStart;
-      if(numImages < 2) {
+      if (numImages < 2) {
         imagePosMap[index++] = `${rowStart}/${colStart}/ span ${rowSpan}/ span ${colSpan}`;
         return;
       }
 
       //row first partition
-      let halfImg = Math.floor(numImages/2);
-      if(partRow) {
-        let newRowEnd = rowStart - 1 + rowSpan/2;
-        part(rowStart, newRowEnd,colStart, colEnd, numImages - halfImg, false);
-        part(newRowEnd+1,rowEnd,colStart, colEnd, halfImg, false);
+      let halfImg = Math.floor(numImages / 2);
+      if (partRow) {
+        let newRowEnd = rowStart - 1 + rowSpan / 2;
+        part(rowStart, newRowEnd, colStart, colEnd, numImages - halfImg, false);
+        part(newRowEnd + 1, rowEnd, colStart, colEnd, halfImg, false);
       } else {
-        let colRowEnd = colStart - 1 + colSpan/2;
-        part(rowStart, rowEnd, colStart, colRowEnd, numImages - halfImg,);
-        part(rowStart, rowEnd, colRowEnd+1, colEnd, halfImg);
+        let colRowEnd = colStart - 1 + colSpan / 2;
+        part(rowStart, rowEnd, colStart, colRowEnd, numImages - halfImg);
+        part(rowStart, rowEnd, colRowEnd + 1, colEnd, halfImg);
       }
     }
 
-    if(numImg > 0) {
-      part(1,n,1,n, numImg);
+    if (numImg > 0) {
+      part(1, n, 1, n, numImg);
       this._imagePosMap = imagePosMap;
     }
   }
+
+  openDialog(hasStories: boolean, data: MonthData) {
+    if (!hasStories) return;
+    const dialogRef = this.dialog.open(MonthStoriesDialog, {
+      enterAnimationDuration: '350ms',
+      exitAnimationDuration: '150ms',
+      minWidth: '300px',
+      width: '80%',
+      maxWidth: '900px',
+      panelClass: "month-stories-dialog",
+      scrollStrategy: this.scroll.scrollStrategies.noop(),
+      data: {
+        stories: data.stories,
+        name: data.name,
+        id: data.id,
+        year: data.year
+      }
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+}
+
+@Component({
+  selector: 'month-stories-dialog',
+  templateUrl: 'month-stories-dialog.html',
+  standalone: true,
+  imports: [MatDialogModule, CommonModule, MatButtonModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class MonthStoriesDialog {
+  readonly data = inject<DialogData>(MAT_DIALOG_DATA);
 }
